49cc556ca19bc5c05de4c09f582c0556711264c1
Hoangkute123xyz <hoangdv@wavez.vn>
2023-08-02 10:30:01 +0700

    update

--- lib/billing/signature_checker.dart
+++ lib/billing/signature_checker.dart
@@ -1,18 +1,12 @@
-import 'package:injectable/injectable.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
-@singleton
-class SignatureChecker {
-  SignatureChecker(this.packageInfo);
-
+abstract class SignatureChecker {
   final PackageInfo packageInfo;
 
-  static const _acceptedSignature = [
-    "9EE1999345B1040C879F0BDB22A7C356BBC5A973",
-    "25E599C439685229E2E3220BE142991E1F02193A",
-    "80C40A3042E4ED9F2A0DFCCD2CD79F688833E65D" /*Hoang's computer debug*/
-  ];
+  List<String> get acceptSignature;
+
+  SignatureChecker(this.packageInfo);
 
   bool validSignature() =>
-      _acceptedSignature.contains(packageInfo.buildSignature);
+      acceptSignature.contains(packageInfo.buildSignature);
 }
--- lib/di/package_di.config.dart
+++ lib/di/package_di.config.dart
@@ -11,11 +11,9 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:flutter_billing/billing/billing_helper.dart' as _i5;
 import 'package:flutter_billing/billing/billing_repository.dart' as _i3;
-import 'package:flutter_billing/billing/signature_checker.dart' as _i6;
 import 'package:flutter_core/data/shared/premium_holder.dart' as _i4;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:package_info_plus/package_info_plus.dart' as _i7;
 
 extension GetItInjectableX on _i1.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -35,8 +33,6 @@ extension GetItInjectableX on _i1.GetIt {
       ),
       dispose: (i) => i.dispose(),
     );
-    gh.singleton<_i6.SignatureChecker>(
-        _i6.SignatureChecker(gh<_i7.PackageInfo>()));
     return this;
   }
 }
