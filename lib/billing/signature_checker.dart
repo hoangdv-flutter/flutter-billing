0a27cb266e715b5f4806afd33de900dd1b6e3af3
Hoangkute123xyz <hoangdv@wavez.vn>
2023-08-24 14:59:19 +0700

    add adjust, and encrypt signature

--- lib/billing/signature_checker.dart
+++ lib/billing/signature_checker.dart
@@ -1,3 +1,4 @@
+import 'package:encrypt/encrypt.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 abstract class SignatureChecker {
@@ -5,8 +6,33 @@ abstract class SignatureChecker {
 
   List<String> get acceptSignature;
 
-  SignatureChecker(this.packageInfo);
+  SignatureChecker(this.packageInfo) {
+    for (var sign in acceptSignature) {
+      final decrypted = decrypt(sign);
+      final encrypted = encrypt(decrypted);
+      print("encrypted: $encrypted");
+      print("decrypted: $decrypted");
+    }
+  }
 
   bool validSignature() =>
-      acceptSignature.contains(packageInfo.buildSignature);
+      acceptSignature.contains(encrypt(packageInfo.buildSignature));
+
+  late final iv = IV.fromLength(8);
+
+  late final encrypter = createEncrypter();
+
+  String encrypt(String data) {
+    return encrypter.encrypt(data, iv: iv).base64;
+  }
+
+  String decrypt(String data) {
+    return encrypter.decrypt64(data, iv: iv);
+  }
+
+  Encrypter createEncrypter() {
+    final key = Key.fromLength(32);
+    final encrypter = Encrypter(Salsa20(key));
+    return encrypter;
+  }
 }
